2e9a46e25c10b6d09c94e62883fac278a81f3f31
Quitterie Cadiou <quitterie.cadiou@gmail.com>
2018-10-17 15:53:01 +0200

    everything except reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '020 7376 1498',
+    category:     'chinese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '077 6722 5551',
+    category:     'italian'
+  },
+  {
+    name:         'Dozo',
+    address:      'Harrington Road, London SW7',
+    phone_number: '077 6722 5599',
+    category:     'japanese'
+  },
+  {
+    name:         'Yauatcha',
+    address:      '15-17 Broadwick St, Soho, London W1F 0DL',
+    phone_number: '077 6722 5551',
+    category:     'chinese'
+  },
+  {
+    name:         'Chotto Matte',
+    address:      '11 - 13 Frith St, Soho, London W1D 4RB',
+    phone_number: '077 6722 5500',
+    category:     'japanese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
